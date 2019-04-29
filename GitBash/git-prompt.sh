4d5a7275feded528c434f19da7f5836cc39b027d
Alexis Pavlidis <apavlidi@it.teithe.gr>
2019-04-29 11:38:06 +0300

    Edit git bash

--- GitBash/git-prompt.sh
+++ GitBash/git-prompt.sh
@@ -1,35 +1,42 @@
-# if test -f /etc/profile.d/git-sdk.sh
-# then
-#     TITLEPREFIX=SDK-${MSYSTEM#MINGW}
-# else
-#     TITLEPREFIX=$MSYSTEM
-# fi
-
-# set window title
-PS1='\[\033]0;Git-Bash: ${PWD//[^[:ascii:]]/?}\007\]'
-PS1="$PS1"'\[\033[36m\]'        # change color to cyan for text before command prompt
-PS1="$PS1"'\n'                  # new line
-PS1="$PS1"'\w '                 # current working directory
-PS1="$PS1"'\[\033[32m\]'        # change to green
-PS1="$PS1"'➜ '                 # ➜
-
-
-#For git 
-if test -z "$WINELOADERNOEXEC"
+if test -f /etc/profile.d/git-sdk.sh
 then
-    GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
-    COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
-    COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
-    COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
-    if test -f "$COMPLETION_PATH/git-prompt.sh"
-    then
-        . "$COMPLETION_PATH/git-completion.bash"
-        . "$COMPLETION_PATH/git-prompt.sh"
-        PS1="$PS1"'\[\033[0m\]'  # change color to cyan
-        PS1="$PS1"'`__git_ps1` '   # bash function
-    fi
+	TITLEPREFIX=SDK-${MSYSTEM#MINGW}
+else
+	TITLEPREFIX=$MSYSTEM
 fi
-# PS1="$PS1"'\[\033[0m\]'        # change color
-# PS1="$PS1"'\n'                 # new line
-# PS1="$PS1"' $ '                 # prompt: always $
+
+MSYSTEM=''
+TITLEPREFIX=''
+
+if test -f ~/.config/git/git-prompt.sh
+then
+	. ~/.config/git/git-prompt.sh
+else
+	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+	PS1="$PS1"'\n'                 # new line
+	PS1="$PS1"'\[\033[32m\]'       # change to green
+	# PS1="$PS1"'\u@\h '             # user@host<space>
+	PS1="$PS1"'\[\033[35m\]'       # change to purple
+	PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
+	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
+	PS1="$PS1"'\w'                 # current working directory
+	if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			. "$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+			PS1="$PS1"'`__git_ps1`'   # bash function
+		fi
+	fi
+	PS1="$PS1"'\[\033[0m\]'        # change color
+	# PS1="$PS1"'\n'                 # new line
+	PS1="$PS1"' ➜ '                 # prompt: always $
+fi
+
 MSYS2_PS1="$PS1"               # for detection by MSYS2 SDK's bash.basrc
